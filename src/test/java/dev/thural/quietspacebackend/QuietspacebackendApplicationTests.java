e43da2c27c821f7edc4bec80c1da889dde2f5d14
thural <tural.musaibov@gmail.com>
2023-11-30 23:56:14 +0400

    initial project setup

---
+++ src/test/java/dev/thural/quietspacebackend/QuietspacebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thural.quietspacebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuietspacebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
