26f54fd9d030ffc8ddfb5fd7ce527cdee861d82f
thural <tural.musaibov@gmail.com>
2023-12-14 14:52:29 +0400

    init JPA entities

---
+++ src/main/java/dev/thural/quietspacebackend/entity/UserEntity.java
@@ -0,0 +1,52 @@
+package dev.thural.quietspacebackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Integer version;
+
+    private String username;
+
+    private String password;
+
+    private List<UserEntity> friendIds;
+
+    private OffsetDateTime createDate;
+
+    private OffsetDateTime updateDate;
+
+    @PrePersist
+    private void onCreate() {
+        createDate = OffsetDateTime.now();
+        updateDate = OffsetDateTime.now();
+    }
+
+    @PreUpdate
+    private void onUpdate() {
+        updateDate = OffsetDateTime.now();
+    }
+
+}
