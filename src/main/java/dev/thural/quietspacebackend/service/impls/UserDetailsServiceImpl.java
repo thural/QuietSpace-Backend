b4e4dfdc12bd3eb1972ae067ac03074503438016
thural <tural.musaibov@gmail.com>
2024-01-23 23:13:11 +0400

    collect user roles to authority list

--- src/main/java/dev/thural/quietspacebackend/service/impls/UserDetailsServiceImpl.java
+++ src/main/java/dev/thural/quietspacebackend/service/impls/UserDetailsServiceImpl.java
@@ -4,6 +4,7 @@ import dev.thural.quietspacebackend.entity.UserEntity;
 import dev.thural.quietspacebackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,8 +12,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -27,7 +30,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         UserEntity user = optionalUser.orElseThrow(
                 () -> new UsernameNotFoundException("user not found with the email"));
 
-        List<GrantedAuthority> authorityList = new ArrayList<>();
+        List<GrantedAuthority> authorityList = Arrays.stream(user.getRole().split(","))
+                .map(role -> new SimpleGrantedAuthority(role.toUpperCase()))
+                .collect(Collectors.toList());
+
         return new User(user.getEmail(),user.getPassword(),authorityList);
     }
 }
