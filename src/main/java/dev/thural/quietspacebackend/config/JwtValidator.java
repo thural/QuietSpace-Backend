d6ca85d78b6d74d25c8781be5c25f144d40f24b4
thural <tural.musaibov@gmail.com>
2024-01-25 21:49:14 +0400

    refactor and simplify jwt filter using userDetailsService

--- src/main/java/dev/thural/quietspacebackend/config/JwtValidator.java
+++ src/main/java/dev/thural/quietspacebackend/config/JwtValidator.java
@@ -1,58 +1,65 @@
 package dev.thural.quietspacebackend.config;
 
 import dev.thural.quietspacebackend.constant.JwtConstant;
-import dev.thural.quietspacebackend.entity.UserEntity;
+import dev.thural.quietspacebackend.exception.UnauthorizedException;
 import dev.thural.quietspacebackend.repository.UserRepository;
 import dev.thural.quietspacebackend.service.TokenBlackList;
 import dev.thural.quietspacebackend.utils.JwtProvider;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class JwtValidator extends OncePerRequestFilter {
     private final TokenBlackList tokenBlackList;
-    private final UserRepository userRepository;
+    private final UserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String jwt = request.getHeader(JwtConstant.JWT_HEADER);
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader(JwtConstant.JWT_HEADER);
 
-        if (StringUtils.hasText(jwt) && jwt.startsWith("Bearer") && !tokenBlackList.isBlacklisted(jwt)) try {
-            String email = JwtProvider.getEmailFromJwtToken(jwt);
+        if (!StringUtils.hasText(authHeader) || !authHeader.startsWith("Bearer")){
+            filterChain.doFilter(request, response);
+            return;
+        }
 
-            Optional<UserEntity> optionalUser = userRepository.findUserEntityByEmail(email);
+        if (tokenBlackList.isBlacklisted(authHeader)){
+            filterChain.doFilter(request, response);
+            return;
+        }
 
-            UserEntity user = optionalUser.orElseThrow(
-                    () -> new UsernameNotFoundException("user not found with the email"));
+        String userEmail = JwtProvider.extractEmailFromAuthHeader(authHeader);
 
-            List<GrantedAuthority> authorityList = Arrays.stream(user.getRole().split(","))
-                    .map(role -> new SimpleGrantedAuthority(role.toUpperCase()))
-                    .collect(Collectors.toList());
+        if (userEmail == null || SecurityContextHolder.getContext().getAuthentication() != null){
+            filterChain.doFilter(request, response);
+            return;
+        }
 
-            Authentication auth = new UsernamePasswordAuthenticationToken(email, null, authorityList);
-            SecurityContextHolder.getContext().setAuthentication(auth);
-        } catch (Exception e) {
-            throw new BadCredentialsException("invalid token");
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+
+        if (JwtProvider.isTokenValid(authHeader, userDetails)) {
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null,
+                    userDetails.getAuthorities());
+
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authToken);
         }
 
         filterChain.doFilter(request, response);
