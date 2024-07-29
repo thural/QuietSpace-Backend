2b4e368319176db2f30a0a00c963d52ff997bb7c
Thural Musaibov <tural.musaibov@outlook.com>
2024-07-29 18:28:49 +0400

    fix mailing on account activation

--- src/main/java/dev/thural/quietspace/service/impls/EmailService.java
+++ src/main/java/dev/thural/quietspace/service/impls/EmailService.java
@@ -39,7 +39,7 @@ public class EmailService {
         if (emailTemplate == null) {
             templateName = "confirm-email";
         } else {
-            templateName = emailTemplate.name();
+            templateName = emailTemplate.getName();
         }
 
         MimeMessage mimeMessage = mailSender.createMimeMessage();
@@ -66,5 +66,7 @@ public class EmailService {
         helper.setText(template, true);
 
         mailSender.send(mimeMessage);
+
+        log.info("sent email to {}", to);
     }
 }
