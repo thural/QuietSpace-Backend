c8d6453595ba841552f44038e30a26cec6a0e4b9
Tural Musaibov <tural.musaibov@gmail.com>
2026-07-10 17:03:47 +0400

    configure Springwolf for WebSocket API documentation
    - Add springwolf docket config (base-package, info) to application.yml
    - Permit /springwolf/** endpoints in SecurityConfig for UI and JSON access

--- src/main/java/dev/thural/quietspace/security/SecurityConfig.java
+++ src/main/java/dev/thural/quietspace/security/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
                                                 "/configuration/security",
                                                 "/swagger-ui/**",
                                                 "/webjars/**",
-                                                "/swagger-ui.html"
+                                                "/swagger-ui.html",
+                                                "/springwolf/**"
                                         )
                                         .permitAll()
                                         .anyRequest()
