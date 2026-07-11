b3e6947199bb1266b4b5fe05de953b75efae0fe7
Tural Musaibov <tural.musaibov@gmail.com>
2026-07-11 16:02:39 +0400

    refactor: remove thymeleaf starter dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.springframework.boot:spring-boot-starter-mail")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.security:spring-security-messaging")
 
     implementation("org.flywaydb:flyway-core")
