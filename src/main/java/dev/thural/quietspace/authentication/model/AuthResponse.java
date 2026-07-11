b4032450c170a800f5b6937889feed0f83721f4d
Tural Musaibov <tural.musaibov@gmail.com>
2026-07-11 13:02:46 +0400

    Tier 3: Fix AuthRequest/Response/RegistrationRequest Jackson constructors and JWT secret key

--- src/main/java/dev/thural/quietspace/authentication/model/AuthResponse.java
+++ src/main/java/dev/thural/quietspace/authentication/model/AuthResponse.java
@@ -1,12 +1,16 @@
 package dev.thural.quietspace.authentication.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
 
     private String accessToken;
