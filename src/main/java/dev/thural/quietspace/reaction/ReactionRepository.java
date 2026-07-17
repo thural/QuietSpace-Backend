27591d3a3c8b9e638eb869ce3d52465505dd2379
Tural Musaibov <tural.musaibov@gmail.com>
2026-07-17 21:25:56 +0400

    Refactor ReactionRepository to use Pageable

--- src/main/java/dev/thural/quietspace/reaction/ReactionRepository.java
+++ src/main/java/dev/thural/quietspace/reaction/ReactionRepository.java
@@ -3,7 +3,7 @@ package dev.thural.quietspace.reaction;
 import dev.thural.quietspace.shared.enums.EntityType;
 import dev.thural.quietspace.shared.enums.ReactionType;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -11,21 +11,21 @@ import java.util.UUID;
 
 public interface ReactionRepository extends JpaRepository<Reaction, UUID> {
 
-    Page<Reaction> findAllByContentId(UUID contentId, PageRequest pageRequest);
+    Page<Reaction> findAllByContentId(UUID contentId, Pageable pageable);
 
-    Page<Reaction> findAllByUserId(UUID userId, PageRequest pageRequest);
+    Page<Reaction> findAllByUserId(UUID userId, Pageable pageable);
 
     boolean existsByContentIdAndUserId(UUID contentId, UUID userId);
 
     Optional<Reaction> findByContentIdAndUserId(UUID commentId, UUID id);
 
-    Page<Reaction> findAllByContentTypeAndUserId(EntityType contentType, UUID userId, PageRequest pageRequest);
+    Page<Reaction> findAllByContentTypeAndUserId(EntityType contentType, UUID userId, Pageable pageable);
 
-    Page<Reaction> findAllByContentIdAndContentType(UUID contentId, EntityType contentType, PageRequest pageRequest);
+    Page<Reaction> findAllByContentIdAndContentType(UUID contentId, EntityType contentType, Pageable pageable);
 
-    Page<Reaction> findAllByUserIdAndContentType(UUID userId, EntityType contentType, PageRequest pageRequest);
+    Page<Reaction> findAllByUserIdAndContentType(UUID userId, EntityType contentType, Pageable pageable);
 
     Integer countByContentIdAndReactionType(UUID contentId, ReactionType reactionType);
 
-    Page<Reaction> findAllByContentIdAndReactionType(UUID contentId, ReactionType reactionType, PageRequest pageRequest);
+    Page<Reaction> findAllByContentIdAndReactionType(UUID contentId, ReactionType reactionType, Pageable pageable);
 }
