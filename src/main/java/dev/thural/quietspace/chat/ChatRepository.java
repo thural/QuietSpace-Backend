a713348b6be1df214adac4cf2b3eaf4321e1faf7
Tural Musaibov <tural.musaibov@gmail.com>
2026-07-17 21:28:56 +0400

    Refactor ChatRepository to use Collection for findAllByUsersIn

--- src/main/java/dev/thural/quietspace/chat/ChatRepository.java
+++ src/main/java/dev/thural/quietspace/chat/ChatRepository.java
@@ -3,11 +3,12 @@ package dev.thural.quietspace.chat;
 import dev.thural.quietspace.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
 public interface ChatRepository extends JpaRepository<Chat, UUID> {
     List<Chat> findAllByUsersId(UUID userId);
 
-    List<Chat> findAllByUsersIn(List<User> userList);
+    List<Chat> findAllByUsersIn(Collection<User> users);
 }
