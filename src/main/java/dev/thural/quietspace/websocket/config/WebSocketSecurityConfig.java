2f894a5ea857a46c76ab1f41d327daa4b1a210d8
Tural Musaibov <tural.musaibov@gmail.com>
2026-07-21 09:48:24 +0400

    feat: enforce authentication on WebSocket connections

--- src/main/java/dev/thural/quietspace/websocket/config/WebSocketSecurityConfig.java
+++ src/main/java/dev/thural/quietspace/websocket/config/WebSocketSecurityConfig.java
@@ -1,8 +1,23 @@
 package dev.thural.quietspace.websocket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 
 @Configuration
+@EnableWebSocketSecurity
 public class WebSocketSecurityConfig {
 
+    @Bean
+    AuthorizationManager<Message<?>> messageAuthorizationManager() {
+        return MessageMatcherDelegatingAuthorizationManager.builder()
+                .simpDestMatchers("/public/**").permitAll()
+                .simpDestMatchers("/app/**").authenticated()
+                .simpDestMatchers("/user/**", "/private/**").authenticated()
+                .anyMessage().denyAll()
+                .build();
+    }
 }
